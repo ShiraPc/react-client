83d217879f32bef586bbf89df4cfa106226c719b
EstiLev <st3281357@gmail.com>
2022-07-26 15:48:48 +0300

    start the code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { RecoilRoot } from "recoil";
+import { User } from "./components/users";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RecoilRoot>
+      <div className="App">
+      <React.Suspense fallback={<p> failed </p>}> 
+        <Menu />
+        <div>
+          {/* בתוך הקומפוננטה Routes 
+          זה המקום להגדיר את הגדרות הניתוב
+        */}
+
+          <Routes>
+            <Route path="" element={<h1>my diary</h1>} />
+            <Route path="/user" element={<AddUser />}>
+            <Route path=":getid" element={<User />} />
+              <Route path=":id" element={< UpdateUser />} />
+            </Route>
+            <Route path="*" element={<h1> A mistake </h1>} />
+          </Routes>
+        </div>
+        </React.Suspense>
+      </div>
+    </RecoilRoot>
   );
 }
 
